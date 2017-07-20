206296221284f7113b3bbf47ce8de13a27850fd8
Dobby <jevo747@yeah.net>
2017-07-20 11:40:42 +0800

    os

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@
 '''
 defind file
 '''
-
+import os
 CONTACT_ONE = 'selfdefind'  #wechat name
 DO_WEBSITE = 'http://selfdefind'
-CODE_PHOTO_PATH = '/Users/selfdefind/temp.png'  #to save screenshot, must be absolute path
+CODE_PHOTO_PATH = os.path.join(os.getcwd(),'temp.png')  #to save screenshot, must be absolute path
 USER_NAME = 'selfdefind'            #username
 PASS_WORD = 'selfdefind'            #password
-HELP_COMMAND = 'forword me command,Example :\ncheckin: checkin\ncheckout: checkout\nci<co>codexxxx: checkin<out> and input verification code,xxxx is the verification code in picture which last one i send.'
+HELP_COMMAND = 'forword me command,Example :\ncheckin: checkin\ncheckout: checkout\ncisxx: checkin<out> and input verification code,xxxx is the verification code in picture which last one i send.'
